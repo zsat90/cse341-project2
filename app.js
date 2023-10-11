d57c2437c3d31198e6f908fbb29d4a7d2b041e56
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2023-10-11 18:19:02 -0400

    render links

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    next();
   });
 
 mongoose.connection.once("open", () => {
